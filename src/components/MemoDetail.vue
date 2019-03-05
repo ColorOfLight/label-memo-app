ff0d4bc799f5ab6a62f7efe01da6efdb836efd6a
Seongho Park <bluetosh7@gmail.com>
2019-03-06 04:23:10 +0900

    feat: add edit mode in memoDetail

---
+++ src/components/MemoDetail.vue
@@ -0,0 +1,185 @@
+<template lang="pug">
+  .memo-detail-container(:class="{edit: isEditMode}")
+    .memo-detail-title-card
+      .memo-detail-title-title 제목 3
+      input.memo-detail-title-input(value="제목 3")
+      .memo-detail-title-labels 라벨 1, 라벨 2
+      .memo-detail-edit-container
+        a.memo-detail-edit-btn(@click="toggleEditMode") 편집하기
+        .memo-detail-editing 편집 중
+    .memo-detail-content-container
+      |본 프로그램은 라벨이 포함된 메모를 편집할 수 있습니다.
+      |내용을 편집하고 싶으시면 편집하기를 눌러 내용을 편집해주세요!
+    textarea.memo-detail-content-textarea
+      |본 프로그램은 라벨이 포함된 메모를 편집할 수 있습니다.
+      |내용을 편집하고 싶으시면 편집하기를 눌러 내용을 편집해주세요!
+    .memo-detail-btn-wrapper
+      a.btn.memo-detail-finish-btn 편집 완료
+
+    a.btn-new-memo
+      i.material-icons edit
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      isEditMode: false,
+    }
+  },
+  methods: {
+    toggleEditMode () {
+      this.isEditMode = !this.isEditMode;
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.memo-detail-container {
+  background-color: $bg-color;
+  height: 100%;
+  padding: 1.5rem;
+  padding-left: $label-list-width + $memo-list-width + 1.5rem;
+  width: 100%;
+  z-index: $z-memo-detail;
+
+  .memo-detail-title-card {
+    background-color: $white;
+    border-bottom-left-radius: 1rem;
+    border-bottom-right-radius: 1rem;
+    box-shadow: $card-box-shadow;
+    padding: 1rem 1.5rem;
+    position: relative;
+  }
+
+  .memo-detail-title-title {
+    font-size: 2rem;
+    font-weight: 800;
+  }
+
+  .memo-detail-title-labels {
+    color: $gray;
+    margin-top: .125rem;
+  }
+
+  .memo-detail-edit-container {
+    margin-top: .5rem;
+    position: relative;
+    text-align: right;
+  }
+
+  .memo-detail-edit-btn {
+    color: $secondary;
+    font-weight: 600;
+    display: inline-block;
+
+    &:hover {
+      color: darken($secondary, 10%);
+      cursor: pointer;
+    }
+  }
+
+  .memo-detail-editing {
+    color: $gray-light;
+    font-weight: 600;
+    display: none;
+  }
+
+  .memo-detail-content-container,
+  .memo-detail-content-textarea {
+    background-color: $white;
+    border: 1px solid $border-color;
+    font-size: 1rem;
+    line-height: 1.5;
+    margin-top: -1rem;
+    padding: 2.5rem 1.5rem 1.5rem;
+    width: 100%;
+  }
+
+  .memo-detail-content-textarea {
+    display: none;
+    margin-top: .5rem;
+    padding-top: 1.5rem;
+  }
+
+  .btn-new-memo {
+    background-color: $primary;
+    border-radius: 2rem;
+    bottom: 2rem;
+    box-shadow: $btn-box-shadow;
+    color: $white;
+    height: 4rem;
+    padding: 1rem;
+    position: absolute;
+    right: 2.5rem;
+    width: 4rem;
+
+    .material-icons {
+      font-size: 2rem;
+    }
+  }
+
+  .memo-detail-title-input {
+    border: 0;
+    border-bottom: 2px solid $gray-lighter;
+    font-size: 2rem;
+    font-weight: 800;
+    display: none;
+    margin-bottom: .75rem;
+    padding-bottom: .125rem;
+  }
+
+  .memo-detail-btn-wrapper {
+    margin-top: 1rem;
+    text-align: right;
+  }
+
+  .memo-detail-finish-btn {
+    background-color: $white;
+    border: 1px solid $primary;
+    border-radius: .5rem;
+    color: $primary;
+    display: none;
+    font-weight: 500;
+    line-height: 2.5rem;
+    height: 2.5rem;
+    padding: 0 2rem;
+    text-align: center;
+
+    &:hover {
+      background-color: $primary-bg;
+    }
+  }
+}
+
+.memo-detail-container.edit {
+  .memo-detail-title-title {
+    display: none;
+  }
+
+  .memo-detail-title-input {
+    display: block;
+  }
+
+  .memo-detail-edit-btn {
+    display: none;
+  }
+
+  .memo-detail-editing {
+    display: inline-block;
+  }
+
+  .memo-detail-content-container {
+    display: none;
+  }
+
+  .memo-detail-content-textarea {
+    display: block;
+  }
+
+  .memo-detail-finish-btn {
+    display: inline-block;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,27 +2,19 @@
   .page-home
     label-list
     memo-list
-    .memo-detail-container
-      .memo-detail-title-card
-        .memo-detail-title-title 제목 3
-        .memo-detail-title-labels 라벨 1, 라벨 2
-        .memo-detail-edit-container
-          .memo-detail-edit-btn 편집하기
-      .memo-detail-content-container
-        |본 프로그램은 라벨이 포함된 메모를 편집할 수 있습니다.
-        |내용을 편집하고 싶으시면 편집하기를 눌러 내용을 편집해주세요!
-      a.btn-new-memo
-        i.material-icons edit
+    memo-detail
 </template>
 
 <script>
 import LabelList from "@/components/LabelList";
 import MemoList from "@/components/MemoList";
+import MemoDetail from "@/components/MemoDetail";
 
 export default {
   components: {
     'label-list': LabelList,
     'memo-list': MemoList,
+    'memo-detail': MemoDetail,
   }
 }
 </script>
@@ -32,70 +24,5 @@ export default {
   height: 100vh;
   position: relative;
   width: 100%;
-}
-
-.memo-detail-container {
-  background-color: $bg-color;
-  height: 100%;
-  padding: 1.5rem;
-  padding-left: $label-list-width + $memo-list-width + 1.5rem;
-  width: 100%;
-  z-index: $z-memo-detail;
-
-  .memo-detail-title-card {
-    background-color: $white;
-    border-bottom-left-radius: 1rem;
-    border-bottom-right-radius: 1rem;
-    box-shadow: $card-box-shadow;
-    padding: 1rem 1.5rem;
-    position: relative;
-  }
-
-  .memo-detail-title-title {
-    font-size: 2rem;
-    font-weight: 800;
-  }
-
-  .memo-detail-title-labels {
-    color: $gray;
-    margin-top: .125rem;
-  }
-
-  .memo-detail-edit-container {
-    margin-top: .5rem;
-    position: relative;
-    text-align: right;
-  }
-
-  .memo-detail-edit-btn {
-    color: $secondary;
-    font-weight: 600;
-    display: inline-block;
-  }
-
-  .memo-detail-content-container {
-    background-color: $white;
-    border: 1px solid $border-color;
-    line-height: 1.5;
-    margin-top: -1rem;
-    padding: 2.5rem 1.5rem 1.5rem;
-  }
-
-  .btn-new-memo {
-    background-color: $primary;
-    border-radius: 2rem;
-    bottom: 2rem;
-    box-shadow: $btn-box-shadow;
-    color: $white;
-    height: 4rem;
-    padding: 1rem;
-    position: absolute;
-    right: 2.5rem;
-    width: 4rem;
-
-    .material-icons {
-      font-size: 2rem;
-    }
-  }
 }
 </style>
