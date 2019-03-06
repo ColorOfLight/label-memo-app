5d2601478b544777ac47a58a14064b6b90eee92d
Seongho Park <bluetosh7@gmail.com>
2019-03-06 13:27:51 +0900

    feat: connect with firebase & add auth feat

--- src/main.js
+++ src/main.js
@@ -3,17 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import firebase from 'firebase'
-
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyBz6nbUElbP-SKOaqnpWwEqrc2uDyPM4HU",
-  authDomain: "label-memo-app.firebaseapp.com",
-  databaseURL: "https://label-memo-app.firebaseio.com",
-  // storageBucket: "<BUCKET>.appspot.com",
-};
-firebase.initializeApp(config);
-
 Vue.config.productionTip = false
 
 new Vue({
